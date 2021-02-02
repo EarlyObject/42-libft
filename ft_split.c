53a56c1756f368f03eda6ce36431923066c50b30
EarlyObject <sr.ardak@gmail.com>
2021-02-02 15:45:29 +0100

    ft_split draft

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 09:10:17 by asydykna          #+#    #+#             */
-/*   Updated: 2021/02/01 23:05:39 by asydykna         ###   ########.fr       */
+/*   Updated: 2021/02/02 15:42:39 by asydykna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ char **ft_split(char const *s, char c)
 	size_t columns;
 	size_t i;
 	size_t j;
-
+	size_t totalcolumns;
+	//	char **backup;
 	temp = NULL;
 	final = NULL;
 	rows = 0;
+	totalcolumns = 0;
 	while (*s)
 	{
 		columns = 0;
@@ -37,49 +39,61 @@ char **ft_split(char const *s, char c)
 			s++;
 		}
 		rows++;
-		temp = ft_calloc(rows + 1, sizeof(s));
-		i = 0;
-		while (i < rows - 1)
-		{
-			j = ft_strlen(&final[i][0]);
-			temp[i] = ft_calloc(j + 1, sizeof(s));
-			memcpy(&temp[i][0], &final[i][0], j);
-			i++;
-		}
-		temp[rows - 1] = ft_calloc(columns + 1, sizeof(s));
-		memcpy(&temp[rows - 1][0], s - columns, columns);
+		totalcolumns += columns;
+		printf("rows: %zu\n", rows);
+		printf("columns: %zu\n", columns);
+		temp = (char**)malloc((rows + 1) * sizeof(s));
 
-		final = temp;
+		i = 0;
+		while (i < (rows - 1))
+		{
+			j = ft_strlen(final[i]);
+			printf("j = %zu\n", j);
+			if (rows > 1)
+			{
+				for (size_t i = 0; i < rows; i++)
+					printf("\nbefore malloc: final %zu is %s", i, final[i]);
+		}
+		
+		//problem here!
+		temp[i] = (char *)malloc((j + 1) * sizeof(s));
+
+		printf("\nafter malloc: final %zu is %s\n", i, final[i]);
+		memmove(temp[i], final[i], j + 1);
+		temp[i][columns + 1] = '\0';
+		printf("normal line: %s\n", temp[i]);
+		i++;
+		}
+		temp[rows - 1] = (char*)malloc((columns + 1) * sizeof(s));
+		memcpy(temp[rows - 1], s - columns, columns+1);
+		temp[rows - 1][columns] = '\0';
+		printf("final line: %s\n", temp[rows - 1]);
+
+		temp[rows] = NULL;
+		int len = (rows + 1) * (columns + rows);
+		final = (char **)malloc(len *sizeof(s));
+		memcpy(final, temp, len);
+		
+		for (size_t i = 0; i < rows; i++)
+		{
+			printf("after while: i = %zu, %s\n", i, final[i]);
+		}
+
+		/* char **arr2d2 = final;
+    	int itr = 0;
+		printf("Content of final:\n");
+		while (arr2d2[itr] != NULL)
+		{
+        printf("%s\n", arr2d2[itr]);
+        itr++;
+    	}
+		printf("End of content\n"); */
+
+		
 		free(temp);
 		if (*s == '\0')
 			break;
 	}
+	printf("\n\n");
 	return final;
 }
-
-/* while (*s)
-	{
-		columns = 0;
-		while (*s == c)
-			s++;
-		while (*s && *s != c)
-		{
-			columns++;
-			s++;
-		}
-		rows++;
-		temp = ft_calloc(rows + 1, sizeof(s));
-		i = 0;
-		while (i < rows - 1)
-		{
-			j = ft_strlen(&final[i][0]);
-			temp[i] = ft_calloc(j + 1, sizeof(s));
-			memcpy(&temp[i][0], &final[i][0], j);
-			i++;
-		}
-		temp[rows - 1] = ft_calloc(columns + 1, sizeof(s));
-		memcpy(&temp[rows - 1][0], s - columns, columns);
-		s++;
-		final = temp;
-		free(temp);
-	} */
